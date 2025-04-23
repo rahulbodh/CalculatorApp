e5d62ddc39d960c996aff4d5212bcbfc11d68d8c
Rahul <rahulbodh819@gmail.com>
2025-04-23 19:01:51 +0530

    calculator ui

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -42,6 +52,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Calculator() {
+    var display by remember { mutableStateOf("0") }
+    var operand by remember { mutableStateOf("") }
+    var operator by remember { mutableStateOf("") }
+    var isNewNumber by remember { mutableStateOf(true) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -65,6 +80,44 @@ fun Calculator() {
                 .padding(16.dp),
             textAlign = TextAlign.End
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        val buttons = listOf(
+            listOf("C", "+/-", "%", "/"),
+            listOf("7", "8", "9", "x"),
+            listOf("4", "5", "6", "-"),
+            listOf("1", "2", "3", "+"),
+            listOf("0", ".", "=")
+        )
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ){
+            buttons.forEach { row ->
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    row.forEach { button ->
+                        val weight = if(button == "0") 2f else 1f
+                       Button(
+                           onClick = { },
+                           modifier = Modifier
+                               .weight(weight)
+                               .aspectRatio(if(button == "0") 2f else 1f),
+                           shape = RoundedCornerShape(8.dp)
+                       ) {
+                           Text(
+                               text = button,
+                               fontSize = 24.sp,
+                               fontWeight = FontWeight.Medium
+                           )
+                       }
+                    }
+                }
+            }
+        }
     }
 
 }
